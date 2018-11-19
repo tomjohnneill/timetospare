cb57e9f18c50836367c4a8470ffa707b41ca7155
Tom Neill <tomjohnneill@live.co.uk>
2018-11-19 20:35:01 +0000

    Added back in document.js

---
+++ src/app/pages/_document.js
@@ -0,0 +1,48 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+    return { html, head, errorHtml, chunks };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link href="https://fonts.googleapis.com/css?family=Nunito:200,500,700" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet"/>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `}}
+          />
+        </Head>
+        <body
+          style={{
+            margin: 0,
+            padding: 0,
+            fontFamily: 'Nunito',
+            color: 'black',
+            textDecoration: 'none',
+            listStyleType: 'none'
+          }}
+        className='body'>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
